4d8c4907f61678ac1b3243319ecd089f40393be5
Martin_Lewiz <martin93fci@gmail.com>
2016-05-06 23:48:12 +0200

    first Commit

---
+++ Security-Project-master/Security/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Security
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Browse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ECB = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.OFB = new System.Windows.Forms.Button();
+            this.CFB = new System.Windows.Forms.Button();
+            this.CBC = new System.Windows.Forms.Button();
+            this.ChangeKey = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.ChnageIV = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(553, 12);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(162, 32);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse BMP Gray Image";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(452, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(288, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(262, 268);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ECB
+            // 
+            this.ECB.Location = new System.Drawing.Point(697, 90);
+            this.ECB.Name = "ECB";
+            this.ECB.Size = new System.Drawing.Size(101, 32);
+            this.ECB.TabIndex = 0;
+            this.ECB.Text = "View ECB Image";
+            this.ECB.UseVisualStyleBackColor = true;
+            this.ECB.Click += new System.EventHandler(this.ECB_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(697, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "View CTR Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // OFB
+            // 
+            this.OFB.Location = new System.Drawing.Point(697, 246);
+            this.OFB.Name = "OFB";
+            this.OFB.Size = new System.Drawing.Size(101, 32);
+            this.OFB.TabIndex = 4;
+            this.OFB.Text = "View OFB Image";
+            this.OFB.UseVisualStyleBackColor = true;
+            this.OFB.Click += new System.EventHandler(this.OFB_Click);
+            // 
+            // CFB
+            // 
+            this.CFB.Location = new System.Drawing.Point(697, 194);
+            this.CFB.Name = "CFB";
+            this.CFB.Size = new System.Drawing.Size(101, 32);
+            this.CFB.TabIndex = 5;
+            this.CFB.Text = "View CFB Image";
+            this.CFB.UseVisualStyleBackColor = true;
+            this.CFB.Click += new System.EventHandler(this.CFB_Click);
+            // 
+            // CBC
+            // 
+            this.CBC.Location = new System.Drawing.Point(697, 142);
+            this.CBC.Name = "CBC";
+            this.CBC.Size = new System.Drawing.Size(101, 32);
+            this.CBC.TabIndex = 6;
+            this.CBC.Text = "View CBC Image";
+            this.CBC.UseVisualStyleBackColor = true;
+            this.CBC.Click += new System.EventHandler(this.CBC_Click);
+            // 
+            // ChangeKey
+            // 
+            this.ChangeKey.Location = new System.Drawing.Point(51, 324);
+            this.ChangeKey.Name = "ChangeKey";
+            this.ChangeKey.Size = new System.Drawing.Size(127, 32);
+            this.ChangeKey.TabIndex = 7;
+            this.ChangeKey.Text = "Change Key";
+            this.ChangeKey.UseVisualStyleBackColor = true;
+            this.ChangeKey.Click += new System.EventHandler(this.ChangeKey_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(444, 324);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(95, 32);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Close";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // ChnageIV
+            // 
+            this.ChnageIV.Location = new System.Drawing.Point(220, 324);
+            this.ChnageIV.Name = "ChnageIV";
+            this.ChnageIV.Size = new System.Drawing.Size(136, 32);
+            this.ChnageIV.TabIndex = 9;
+            this.ChnageIV.Text = "Change IV";
+            this.ChnageIV.UseVisualStyleBackColor = true;
+            this.ChnageIV.Click += new System.EventHandler(this.ChnageIV_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 368);
+            this.Controls.Add(this.ChnageIV);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.ChangeKey);
+            this.Controls.Add(this.CBC);
+            this.Controls.Add(this.CFB);
+            this.Controls.Add(this.OFB);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ECB);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Text = "Des Block Modes Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button ECB;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button OFB;
+        private System.Windows.Forms.Button CFB;
+        private System.Windows.Forms.Button CBC;
+        private System.Windows.Forms.Button ChangeKey;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button ChnageIV;
+    }
+}
+
---
+++ Security-Project-master/Security/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Image Files(*.bmp)|*.bmp";
+                dialog.Title = "Select a BMP image";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = textBox1.Text = dialog.FileName;
+                    pictureBox1.Image = Image.FromFile(fileName);
+                }
+            }
+
+            catch (Exception)
+            {
+                throw new ApplicationException("Failed loading Image");
+            }
+        }
+
+        //Create instance of DES
+        DESCryptoServiceProvider des = new DESCryptoServiceProvider();            
+        
+        Random rand = new Random();
+
+        //des.GenerateKey();
+        static byte[] sKey = new byte[8];
+        //des.GenerateIV();
+        static byte[] sIV = new byte[8];
+
+        
+        // ECB MODE //
+        private void ECB_Click(object sender, EventArgs e)
+        {
+            
+            //Generate IV and Key
+
+
+            //Set Encryption mode
+            des.Mode = CipherMode.ECB;
+            
+            //Read
+            FileStream filestream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);            
+            MemoryStream ms = new MemoryStream();
+            filestream.CopyTo(ms);
+            
+            //Store header in byte array (we will used this after encryption)
+            var header = ms.ToArray().Take(54).ToArray();
+
+            //Take rest from stream
+            var imageArray = ms.ToArray().Skip(54).ToArray();
+
+            //Create encryptor
+            var enc = des.CreateEncryptor();
+
+            //Encrypt image
+            var encimg = enc.TransformFinalBlock(imageArray, 0, imageArray.Length);
+
+            //Combine header and encrypted image
+            var image = Combine(header, encimg);
+            
+            //Specify output file path for encrypted image
+            //string fileName = @"C:\Users\Public\Pictures\Sample Pictures\ECB.bmp";
+            MemoryStream mss = new MemoryStream();
+            mss.Write(image, 0, image.Length);
+
+            //Write encrypted image to disk
+            //File.WriteAllBytes(fileName, image);
+
+            //Show image from disk to pictureBox
+            pictureBox2.Image = Image.FromStream(mss, false);
+            
+        }
+
+
+
+
+        public static byte[] Combine(byte[] first, byte[] second)
+        {
+            byte[] ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+
+        /*public static byte[] YourKey(byte[] sKey)
+        {
+            
+        }*/
+
+        /*public static byte[] YourIV (byte[] sIV)
+          {
+          }*/
+
+
+        //CBC Mode
+
+        private void CBC_Click(object sender, EventArgs e)
+        {
+
+            //Set Encryption mode
+            des.Mode = CipherMode.CBC;
+
+            //Read
+            FileStream filestream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            MemoryStream ms = new MemoryStream();
+            filestream.CopyTo(ms);
+
+            //Store header in byte array (we will used this after encryption)
+            var header = ms.ToArray().Take(54).ToArray();
+
+            //Take rest from stream
+            var imageArray = ms.ToArray().Skip(54).ToArray();
+
+            //Create encryptor
+            var enc = des.CreateEncryptor();
+
+            //Encrypt image
+            var encimg = enc.TransformFinalBlock(imageArray, 0, imageArray.Length);
+
+            //Combine header and encrypted image
+            var image = Combine(header, encimg);
+
+            //Specify output file path for encrypted image
+            //string fileName = @"C:\Users\Public\Pictures\Sample Pictures\CBC.bmp";
+            //FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            
+            //Write encrypted image to disk
+            //File.WriteAllBytes(fileName, image);
+            MemoryStream mss = new MemoryStream();
+            mss.Write(image, 0, image.Length);
+
+            //Show image from disk to pictureBox
+            pictureBox2.Image = Image.FromStream(mss);
+            filestream.Close();
+        }
+
+
+        //CFB Mode
+
+        private void CFB_Click(object sender, EventArgs e)
+        {
+
+
+
+            //Set Encryption mode
+            des.Mode = CipherMode.CFB;
+
+            //Read
+            FileStream filestream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            MemoryStream ms = new MemoryStream();
+            filestream.CopyTo(ms);
+
+            //Store header in byte array (we will used this after encryption)
+            var header = ms.ToArray().Take(54).ToArray();
+
+            //Take rest from stream
+            var imageArray = ms.ToArray().Skip(54).ToArray();
+
+            //Create encryptor
+            var enc = des.CreateEncryptor();
+
+            //Encrypt image
+            var encimg = enc.TransformFinalBlock(imageArray, 0, imageArray.Length);
+
+            //Combine header and encrypted image
+            var image = Combine(header, encimg);
+
+            //Specify output file path for encrypted image
+            //string ENCFILENAME = @"C:\Users\Public\Pictures\Sample Pictures\CFB.bmp";
+
+            //Write encrypted image to disk
+            //File.WriteAllBytes(ENCFILENAME, image);
+            MemoryStream mss = new MemoryStream();
+            mss.Write(image, 0, image.Length);
+
+            //Show image from disk to pictureBox
+            pictureBox2.Image = Image.FromStream(mss);
+            filestream.Close();
+        }
+
+        private void OFB_Click(object sender, EventArgs e)
+        {
+
+            /*rand.NextBytes(sKey);
+            des.Key = sKey;
+
+            rand.NextBytes(sIV);
+            des.IV = sIV;*/
+
+            /*for (int i = 0; i < 50; i++)
+            {
+                
+            }*/
+
+            //Set Encryption mode
+            des.Mode = CipherMode.OFB;
+
+            des.GenerateKey();
+            des.GenerateIV();
+
+
+            //Read
+            FileStream filestream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            MemoryStream ms = new MemoryStream();
+            filestream.CopyTo(ms);
+
+            //Store header in byte array (we will used this after encryption)
+            var header = ms.ToArray().Take(54).ToArray();
+
+            //Take rest from stream
+            var imageArray = ms.ToArray().Skip(54).ToArray();
+
+            //Create encryptor
+            var enc = des.CreateEncryptor();
+
+            //Encrypt image
+            var encimg = enc.TransformFinalBlock(imageArray, 0, imageArray.Length);
+
+            //Combine header and encrypted image
+            var image = Combine(header, encimg);
+
+            //Specify output file path for encrypted image
+            string ENCFILENAME = @"C:\Users\Public\Pictures\Sample Pictures\OFB.bmp";
+
+            //Write encrypted image to disk
+            File.WriteAllBytes(ENCFILENAME, image);
+
+            //Show image from disk to pictureBox
+            pictureBox2.Image = Image.FromFile(ENCFILENAME);
+            filestream.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChangeKey_Click(object sender, EventArgs e)
+        {
+            /*
+            Form2 f1 = new Form2();
+            f1.Show();
+             */
+            rand.NextBytes(sKey);
+            des.Key = sKey;
+
+        }
+
+        private void ChnageIV_Click(object sender, EventArgs e)
+        {
+            /*
+            Form3 f1 = new Form3();
+            f1.Show();
+            */
+            rand.NextBytes(sIV);
+            des.IV = sIV;
+        }
+
+
+        public void SetKey()
+        {
+            sKey[0]=134;sKey[1]=113;sKey[2]=61;
+            sKey[3]=240;sKey[4]=230;sKey[5]=162;
+            sKey[6]=11;sKey[7]=195;
+            des.Key = sKey;
+        }
+
+
+        public void SetIV()
+        {
+            sIV[0]=144;sIV[1]=162;sIV[2]=216;sIV[3]=208;
+            sIV[4]=111;sIV[5]=41;sIV[6]=76;sIV[7]=171;
+            des.IV = sIV;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetKey();
+            SetIV();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream filestream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            MemoryStream ms = new MemoryStream();
+            filestream.CopyTo(ms);
+
+            //Store header in byte array (we will used this after encryption)
+            var header = ms.ToArray().Take(54).ToArray();
+
+
+            byte[] Counter = new byte[8];
+            SetCounter(Counter);
+
+            MemoryStream mss = new MemoryStream();
+
+            byte[] PlainImage= ms.ToArray();
+            int blocks = PlainImage.Length/8;
+
+            for (int i = 0; i < blocks; i++)
+            {
+                /*if (i == blocks - 1)
+                {
+                    var block = ms.ToArray().Take(8).ToArray();
+                }*/
+                var enc = des.CreateEncryptor();
+                var encimg = enc.TransformFinalBlock(Counter, 0, Counter.Length);
+                var block = ms.ToArray().Take(8).ToArray();
+                byte[] Cipher = XOR(block, Counter);
+                mss.Write(Cipher,0,Cipher.Length);
+                IncrementCounter(Counter);
+            }
+
+            //Create encryptor
+            //Encrypt Counter    
+
+
+            
+            //Take rest from stream
+            var imageArray = ms.ToArray().Skip(54).ToArray();
+
+            
+            //Combine header and encrypted image
+            var cip = mss.ToArray();
+            var image = Combine(header, cip);
+
+            //Specify output file path for encrypted image
+            //string fileName = @"C:\Users\Public\Pictures\Sample Pictures\ECB.bmp";           
+            MemoryStream finalIMG = new MemoryStream();
+            finalIMG.Write(image, 0, image.Length);
+
+            //Write encrypted image to disk
+            //File.WriteAllBytes(fileName, image);
+
+            //Show image from disk to pictureBox
+            pictureBox2.Image = Image.FromStream(finalIMG, false);            
+        }
+
+
+        public void SetCounter(byte[] Counter)
+        {
+            Counter[0] = 144; Counter[1] = 162; Counter[2] = 216; Counter[3] = 208;
+            Counter[4] = 111; Counter[5] = 41; Counter[6] = 76; Counter[7] = 171;
+
+        }
+
+        public void IncrementCounter(byte[] Counter)
+        {
+            for (int i = 0; i < Counter.Length; i++)
+            {
+                Counter[i] += 1;
+            }
+        }
+
+        public byte[] XOR(byte[] Block, byte[] Counter)
+        {
+            for (int i = 0; i < Block.Length; i++)
+                Block[i] ^= Counter[i];
+            return Block;
+        }
+
+
+    }
+}
